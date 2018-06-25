7f5f07df9fc474a8c32699d280bf1643f2e23765
Bosko Ivkovic <bosko2003414@gmail.com>
2018-06-25 20:12:24 +0200

    Pushy

---
+++ Assets/_Scripts/PlayerScript.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed;
+    public float sideways;
+    public Animator anim;
+    
+	void Start ()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+	}
+	
+	void Update ()
+    {
+        Movement();
+	}
+
+    void Movement()
+    {
+        float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float rotation = Input.GetAxis("Horizontal") * sideways * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+
+            }
+        }
+
+        transform.Translate(Vector3.forward * translation);
+        transform.Translate(Vector3.right * rotation);
+    }
+
+}
