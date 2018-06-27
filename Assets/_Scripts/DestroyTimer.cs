5ee928594d54c5e9c62aa71cbc528625b6e5968c
Bosko Ivkovic <bosko2003414@gmail.com>
2018-06-27 13:12:21 +0200

    Mags droppen/Destroy after 30sec

---
+++ Assets/_Scripts/DestroyTimer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTimer : MonoBehaviour {
+
+    public float timer;
+    
+	void Start ()
+    {
+        Destroy(this.gameObject, timer);
+	}
+
+}
